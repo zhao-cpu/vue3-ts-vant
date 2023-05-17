239b6a95cceef24bbf70ff2374c8ebd967434866
赵先生 <53158765+zhao-cpu@users.noreply.github.com>
2023-05-17 18:02:12 +0800

    feat: utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -142,3 +142,34 @@ export const getScrollTop = (): number => {
 	}
 	return scrollPos;
 };
+
+/**
+ * 阻止默认下拉回弹效果
+ */
+export const preventTouchmove = () => {
+	document.body.addEventListener(
+		"touchmove",
+		function (e) {
+			e.preventDefault();
+		},
+		{ passive: false }
+	);
+};
+
+// 经纬度计算距离 单位/米
+export const GetDistance = (lat1: number, lng1: number, lat2: number, lng2: number) => {
+	var radLat1 = (lat1 * Math.PI) / 180.0;
+	var radLat2 = (lat2 * Math.PI) / 180.0;
+	var a = radLat1 - radLat2;
+	var b = (lng1 * Math.PI) / 180.0 - (lng2 * Math.PI) / 180.0;
+	var s =
+		2 *
+		Math.asin(
+			Math.sqrt(
+				Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)
+			)
+		);
+	s = s * 6378.137;
+	s = Math.round(s * 10000) / 10;
+	return s;
+};
